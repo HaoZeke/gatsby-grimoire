11ce2c97ca4dbf78691eb6dffe213e801c5044be
HaoZeke <rohit.goswami@aol.com>
2018-11-17 03:01:12 +0530

    content: Restructure and add markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    // Generic
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -17,6 +18,23 @@ module.exports = {
         path: `${__dirname}/src/pages/`,
       },
     },
+    // Org Mode Files
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `org`,
+        path: `${__dirname}/content/org/`,
+      },
+    },
+      // Markdown Files
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `md`,
+        path: `${__dirname}/content/md/`,
+      },
+    },
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-transformer-orga`,
       options: {
