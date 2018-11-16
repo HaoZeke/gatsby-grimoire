f7f795ca8d748da122a6f3f8ab0945eb143c66ee
HaoZeke <rohit.goswami@aol.com>
2018-11-17 02:27:53 +0530

    typography: Use Kirkham

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,14 +1,14 @@
 import Typography from 'typography'
-import theme from 'typography-theme-github'
+import kirkhamTheme from 'typography-theme-kirkham'
 import CodePlugin from 'typography-plugin-code'
 
-theme.baseFontSize = '14px'
-theme.baseLineHeight = 1.45
-theme.scaleRatio = 2.25
-theme.plugins = [
+kirkhamTheme.baseFontSize = '14px'
+kirkhamTheme.baseLineHeight = 1.45
+kirkhamTheme.scaleRatio = 2.25
+kirkhamTheme.plugins = [
   new CodePlugin(),
 ]
-const typography = new Typography(theme)
+const typography = new Typography(kirkhamTheme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
