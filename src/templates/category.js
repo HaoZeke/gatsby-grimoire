d9f63bb716b4161639b21728d5ecaee793ed6954
HaoZeke <rohit.goswami@aol.com>
2018-11-17 06:18:35 +0530

    category: Initialize from plugin

---
+++ src/templates/category.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import PostListing from "../components/PostListing/PostListing";
+
+export default class TagTemplate extends React.Component {
+  render() {
+    const { pageContext, data } = this.props;
+    const { category } = pageContext;
+    return (
+      <Layout>
+        <div className="tag-container">
+          <Helmet title={`Posts in category "${category}"`} />
+          <PostListing postEdges={data.allMarkdownRemark.edges} />
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export const pageQuery = graphql`
+  query CategoryPage($category: String) {
+    allMarkdownRemark(
+      limit: 1000
+      filter: { frontmatter: { category: { eq: $category } } }
+    ) {
+      totalCount
+      edges {
+        node {
+          fields {
+            slug
+          }
+          excerpt
+          timeToRead
+          frontmatter {
+            title
+            tags
+            date
+          }
+        }
+      }
+    }
+  }
+`;
